4c9df951777ed414d25fdba07a092ab34438c565
DimoLitov <dimo_litov@abv.bg>
2024-07-29 22:24:55 +0300

    Adding yaml file5

--- TestProject1/TestCalculator.cs
+++ TestProject1/TestCalculator.cs
@@ -27,7 +27,7 @@ namespace TestProject1
             options.AddArguments("disable-gpu");
             options.AddArguments("window-size=1920x1080");
             options.AddArguments("disable-extensions");
-            options.AddArguments("remote-debugging-port=9222");
+            //options.AddArguments("remote-debugging-port=9222");
 
             driver = new ChromeDriver(options);
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
